4ffb0c9d7bc0ebe03d9e42eb5d39afc03dd7df59
Ludvig Basmajyan <lbasmajyan@gmail.com>
2014-11-10 10:47:05 -0800

    Mario (End of 11/10)
    Made a new map.

--- public_html/lib/resources.js
+++ public_html/lib/resources.js
@@ -15,7 +15,7 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-        {name: "Ludvig - Level 01", type: "tmx", src: "data/map/Ludvig - Level 01.tmx"}
+
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
