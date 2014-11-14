ffac4fa95a526700ae4dfa214364b8ef42e2a90a
Ludvig Basmajyan <lbasmajyan@gmail.com>
2014-11-14 09:22:22 -0800

    Mario (End of 11/14)
    Made my character small, made the animation work and I am trying to make the character go left. Also made the floor on the map work.

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -11,19 +11,40 @@ game.PlayerEntity = me.Entity.extend({
                  return (new me.Rect(0, 0, 128, 128)).toPolygon();
              }
         }]);
+    
+        this.renderable.addAnimation("idle", [3]);
+        this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
+        
+        this.renderable.setCurrentAnimation("idle");
         
         this.body.setVelocity(5, 20); 
     },
     
     update: function(delta){
-        if(me.input.isKeyPressed("right")) {
+        if(me.input.isKeyPressed("left")) {
+            this.Xflip(true);
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+        }
+        else if(me.input.isKeyPressed("right")) {
+            this.Xflip(false);
             this.body.vel.x += this.body.accel.x * me.timer.tick;
         }
         else {
             this.body.vel.x = 0;
         }
         
+        if(this.body.vel.x !== 0) {
+            if(!this.renderable.isCurrentAnimation("smallWalk")) {
+                this.renderable.setCurrentAnimation("smallWalk");
+                this.renderable.setAnimationFrame();
+            }
+        }
+        else {
+            this.renderable.setCurrentAnimation("idle");
+        }
+        
         this.body.update(delta);
+        this._super(me.Entity, "update", [delta]);
         return true;
     }
     
