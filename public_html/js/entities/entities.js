ee72a8846325114d680a640b9e75237791ef8a54
Ludvig Basmajyan <lbasmajyan@gmail.com>
2014-12-03 09:20:51 -0800

    Mario (End of 12/3)
    Finished making the bad guys and made it so if you run into him you go back to the menu.

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -59,7 +59,9 @@ game.PlayerEntity = me.Entity.extend({
     },
     
     collideHandler: function(response){
-        
+        if(response.b.type === 'badguy') {
+            me.state.change(me.state.MENU);
+        }
     }
     
 });
@@ -79,4 +81,66 @@ game.LevelTrigger = me.Entity.extend({
         me.state.current().resetPlayer(this.xSpawn, this.ySpawn);
     }
     
+});
+
+game.BadGuy = me.Entity.extend ({
+    init: function(x, y, settings) {
+        this._super(me.Entity, "init", [x, y, {
+             image: "slime",
+             spritewidth: "60",
+             spriteheight: "28",
+             width: 60,
+             height: 28,
+             getShape: function() {
+                 return (new me.Rect(0, 0, 60, 28)).toPolygon();
+             }
+        }]);
+    
+        this.spritewidth = 60;
+        var width = settings.width;
+        x = this.pos.x;
+        this.startX = x;
+        this.endX = x + width - this.spritewidth;
+        this.pos.X = x + width - this.spritewidth;
+        this.updateBounds();
+        
+        this.alwaysUpdate = true;
+        
+        this.walkLeft = false;
+        this.alive = true;
+        this.type = "badguy";
+        
+        this.renderable.addAnimation("run", [0, 1, 2], 80);
+        this.renderable.setCurrentAnimation("run");
+        
+        this.body.setVelocity(4, 6);
+    },
+    
+    update: function(delta) {
+        this.body.update(delta);
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
+        
+        if(this.alive) {
+            if(this.walkLeft && this.pos.x <= this.startX) {
+                this.walkLeft = false;
+            }
+            else if (!this.walkLeft && this.pos.x >= this.endX) {
+                this.walkLeft = true;
+            }
+            
+            this.flipX(!this.walkLeft);
+            this.body.vel.x += (this.walkLeft) ? -this.body.accel.x * me.timer.tick : this.body.accel.x * me.timer.tick;
+            
+        }
+        else {
+            me.game.world.removeChild(this);
+        }
+        
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    collideHandler: function() {
+        
+    }
 });
